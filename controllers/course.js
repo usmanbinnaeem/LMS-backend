6e9cfc74296e895991044b28ca3308d5df1b8f0e
usmanbinnaeem <usmaanbinnaeem@gmail.com>
2022-04-01 23:33:37 +0500

    added videos to s3 bucket

--- controllers/course.js
+++ controllers/course.js
@@ -2,6 +2,7 @@ import AWS from "aws-sdk";
 import { nanoid } from "nanoid";
 import Course from "../models/course";
 import slugify from "slugify";
+import { readFileSync } from "fs";
 
 const awsConfig = {
   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
@@ -93,6 +94,33 @@ export const read = async (req, res) => {
       .populate("instructor", "_id name")
       .exec();
     res.json(course);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const uploadVideo = async (req, res) => {
+  try {
+    const { video } = req.files;
+    if (!video) return res.status(400).send("No Video Found!");
+
+    const params = {
+      Bucket: "lms-course-lessons-video",
+      Key: `${nanoid()}.${video.type.split("/")[1]}`,
+      Body: readFileSync(video.path),
+      ACL: "public-read",
+      ContentType: video.type,
+    };
+
+    //upload to S3
+    S3.upload(params, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.sendStatus(400);
+      }
+      console.log(data);
+      res.send(data);
+    });
   } catch (err) {
     console.log(err);
   }
--- routes/course.js
+++ routes/course.js
@@ -1,16 +1,17 @@
 import express from "express";
-
+import formidable from 'express-formidable'
 const router = express.Router();
 
 // middleware
 import { requireSignin, isInstructor } from "../middlewares";
 
 // controllers
-import { uploadImage, removeImage, createCourse, read } from "../controllers/course";
+import { uploadImage, removeImage, createCourse, read, uploadVideo } from "../controllers/course";
 
 router.post("/course/upload-image", uploadImage);
 router.post("/course/remove-image", removeImage);
 router.post("/course", requireSignin, isInstructor, createCourse);
 router.get("/course/:slug", read);
+router.post("/course/video-upload", requireSignin, formidable(), uploadVideo)
 
 module.exports = router;
